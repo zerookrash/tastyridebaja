38137eabd39f83ac3db9fc22cce7bf4d8eb0b376
MarioVillelaLarraza <mario.v@oblekco.com>
2021-02-19 17:00:29 -0600

    Formulario de reservas modificado

--- src/app/components/detalle-tour/detalle-tour-component/detalle-tour-component.component.ts
+++ src/app/components/detalle-tour/detalle-tour-component/detalle-tour-component.component.ts
@@ -4,7 +4,14 @@ import {
   Params
 } from '@angular/router';
 
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+// Servicios
 import { DataApiService } from '../../../services/data-api.service';
+
+// Sweet Alert
+import Swal from 'sweetalert2';
+
 import { AltasI } from '../../../models/altas.interface';
 
 @Component({
@@ -16,12 +23,30 @@ export class DetalleTourComponentComponent implements OnInit {
 
   constructor(
     private dataApi: DataApiService,
-    private router: ActivatedRoute
+    private router: ActivatedRoute,
+    private dbData: DataApiService 
   ) { 
     window.scroll(0, 0);
+    this.reservasForm = this.createFormGroup();
   }
 
   public tour: AltasI;
+  emailPattern: any = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/;a
+
+   reservasForm: FormGroup;
+
+  createFormGroup() {
+    return new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      adult: new FormControl('', [Validators.required]),
+      childrens: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.minLength(5), Validators.pattern(this.emailPattern)]),
+      phone: new FormControl('', [Validators.required, Validators.minLength(10)]),
+      date: new FormControl('', [Validators.required]),
+      pickUp: new FormControl('', [Validators.required]),
+      transport: new FormControl('', [Validators.required])
+    })
+  }
 
   ngOnInit() {
     const idTour = this.router.snapshot.params['id'];
@@ -32,6 +57,35 @@ export class DetalleTourComponentComponent implements OnInit {
     this.dataApi.getOneTour(idTour).subscribe( tour => {
       this.tour = tour;
     });
+  }
+
+  onResetForm() {
+    this.reservasForm.reset();
+  }
+
+  onSaveForm() {
+    console.log(this.reservasForm.value);
+    if (this.reservasForm.valid) {
+      this.reservasForm.value.title = this.tour.title;
+      this.dbData.saveReservas(this.reservasForm.value);
+      // Sweet Alert
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'We received your inquiry, someone will contact you shortly..',
+        showConfirmButton: false,
+        timer: 4000
+      })
+      // Sweet Alert
+      this.onResetForm();
+    } else {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Oops...',
+        text: 'All fields are required except for the number of children. Please check the other fields.',
+      })
+    }
   }
 
 }
